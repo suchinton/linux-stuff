27c14fb72a6e8fb00aa1014037d1e273db64121c
suchinton <suchintonroxx@gmail.com>
2021-08-30 14:33:50 +0530

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   conf.d/omf.fish
    	new file:   config.fish
    	new file:   fish_variables
    	new file:   functions/fish_prompt.fish
    	new file:   functions/fish_right_prompt.fish

---
+++ configs/fish-stuff/config.fish
@@ -0,0 +1,37 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+neofetch --ascii /home/suchinton/linux-stuff/ascii/robo.txt
+
+#switch between bash and zsh and fish
+alias tobash="sudo chsh $USER -s /bin/bash && echo 'Now log out.'"
+alias tozsh="sudo chsh $USER -s /bin/zsh && echo 'Now log out.'"
+alias tofish="sudo chsh $USER -s /bin/fish && echo 'Now log out.'"
+
+#fix obvious typo's
+alias cd..='cd ..'
+alias pdw="pwd"
+alias udpate='sudo pacman -Syyu'
+alias upate='sudo pacman -Syyu'
+alias updte='sudo pacman -Syyu'
+alias updqte='sudo pacman -Syyu'
+alias upqll="paru -Syu --noconfirm"
+alias upal="paru -Syu --noconfirm"
+
+#alias to get to /home/suchinton/
+alias home='cd /home/suchinton/'
+
+#alias to get to /linux-stuff/
+alias linstuff='cd /home/suchinton/linux-stuff/'
+
+#alias to get to /all\ walls/
+alias allwalls='cd /home/suchinton/Pictures/wallpapers/all\ walls/'
+
+#alias to start spt-cli
+alias s='spt'
+
+#alias to launch vs code in cpp_stuff
+alias ccpp='code /home/suchinton/cpp_stuff/'
+
+
---
+++ configs/fish-stuff/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/home/suchinton/.local/share/omf/themes/random/fish_prompt.fish
