fe771cd1c5b20c5bfc182c8b9098cea7705337f1
suchinton <suchintonroxx@gmail.com>
2021-09-04 16:42:27 +0530

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   configs/neofetch/config.conf
    	modified:   configs/termite/config
    	modified:   scripts/apps_for_arch.sh

--- scripts/apps_for_arch.sh
+++ scripts/apps_for_arch.sh
@@ -9,37 +9,68 @@ echo '### updating...'
 
 echo '###installing your applications from repos and AUR...'
 echo '                                                  '
+
+    #==================Desktop apps and tools===========================
     paru -S --needed --noconfirm discord telegram-desktop 
-    paru -S --needed --noconfirm spotifyd 
-    paru -S --needed --noconfirm neofetch 
-    paru -S --needed --noconfirm htop 
-    paru -S --needed --noconfirm xfce4-terminal 
-    paru -S --needed --noconfirm termite
+    paru -S --needed --noconfirm stremio
     paru -S --needed --noconfirm stacer 
-    #paru -S --needed --noconfirm bitwarden                                    
-    paru -S --needed --noconfirm cavapify-git 
-    paru -S --needed --noconfirm android-sdk-platform-tools 
-    #-----------------fonts------------------------------------
+    paru -S --needed --noconfirm bitwarden          
+    paru -S --needed --noconfirm brave-bin 
+    paru -S --needed --noconfirm visual-studio-code-bin 
+    paru -S --needed --noconfirm octave
+    paru -S --needed --noconfirm webapp-manager    
+    paru -S --needed --noconfirm qtwaw
+    paru -S --needed --noconfirm evolution && evolution-ews
+    paru -S --needed --noconfirm onlyoffice
+    paru -S --needed --noconfirm blanket
+    paru -S --needed --noconfirm baobab
+                          
+
+    #============================fonts===========================
     paru -S --needed --noconfirm apple-fonts 
     paru -S --needed --noconfirm ttf-google-sans 
     paru -S --needed --noconfirm nerd-fonts-jetbrains-mono
-    #----------------------------------------------------------
-    paru -S --needed --noconfirm stremio 
-    paru -S --needed --noconfirm spotify-tui-bin 
-    #_________________________________________________________________________________________________________________________
-    gpg --keyserver keyserver.ubuntu.com --recv-key 8FD3D9A8D3800305A9FFF259D1742AD60D811D58 # used to import key for spotify.
-    #--------------------------------------------------------------------------------------------------------------------------
-    paru -S --needed --noconfirm spotify 
-    paru -S --needed --noconfirm brave 
-    paru -S --needed --noconfirm visual-studio-code-bin 
-    paru -S --needed --noconfirm octave
-    paru -S --needed --noconfirm webapp-manager
-	paru -S --needed --noconfirm base devel
 
-	#----------------------zsh-stuff--------------------------------
-	paru -S --needed --noconfirm oh-my-zsh-plugin-sutosuggestions
+    #===========================Spotify=========================================================================================
+    #__________________________________________________________________________________________________________________________
+        gpg --keyserver keyserver.ubuntu.com --recv-key 8FD3D9A8D3800305A9FFF259D1742AD60D811D58 # used to import key for spotify.
+    #---------------------------------------------------------------------------------------------------------------------------
+    paru -S --needed --noconfirm spotify 
+    paru -S --needed --noconfirm spotifyd 
+    paru -S --needed --noconfirm spotify-tui-bin 
+
+	#=======================shell-stuff================================
+
+    #------------------------fish-------------------------------------
+    paru -S --needed --noconfirm fish
+	
+    #------------------------zsh--------------------------------------
+    paru -S --needed --noconfirm oh-my-zsh-plugin-sutosuggestions
 	paru -S --needed --noconfirm oh-my-zsh-powerline-theme-git
 	paru -S --needed --noconfirm oh-my-zsh-git
+    paru -S --needed --noconfirm zsh-syntax-highlighting
+    paru -S --needed --noconfirm powerline
+    paru -S --needed --noconfirm zsh
+
+    #====================Terminal-Apps & tools========================
+    paru -S --needed --noconfirm neofetch 
+    paru -S --needed --noconfirm htop 
+    paru -S --needed --noconfirm cavapify-git 
+    paru -S --needed --noconfirm android-sdk-platform-tools 
+    paru -S --needed --noconfirm ranger 
+
+    #=========================Terminal-em================================
+    paru -S --needed --noconfirm xfce4-terminal 
+    paru -S --needed --noconfirm termite
+
+    #=========================Timeshift==================================
+    paru -S --needed --noconfirm timeshift-autosnap
+
+    #============================. =======================================
+    paru -S --needed --noconfirm android-sdk-platform-tools 
+    paru -S --needed --noconfirm base devel
+    paru -S --needed --noconfirm gnome-menus  
+
 echo '                                                  '
 
 echo 'Installing Themes and icons....'
@@ -47,7 +78,7 @@ echo 'Installing Themes and icons....'
     #NORDIC
 
     mkdir /home/suchinton/.themes
-    cd /home/suchinton/.themes
+    #cd /home/suchinton/.themes
     #git clone https://github.com/EliverLara/Nordic.git
 
     paru -S --needed --noconfirm whitesur-cursor-theme-git 
